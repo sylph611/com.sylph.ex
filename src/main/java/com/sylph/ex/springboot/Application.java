967fb0411b2a6a21f3b940ed3aaea1186e906e55
sylph611 <ccg0611a@gmail.com>
2020-06-04 01:42:39 +0900

    테스트

--- src/main/java/com/sylph/ex/springboot/Application.java
+++ src/main/java/com/sylph/ex/springboot/Application.java
@@ -3,7 +3,9 @@ package com.sylph.ex.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
