de4aed35ab07dfc335d47fe361754bae26788e03
sylph611 <ccg0611a@gmail.com>
2020-06-04 18:21:49 +0900

    테스트

--- src/main/java/com/sylph/ex/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/sylph/ex/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.sylph.ex.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     // 인터페이스 생성후 JpaRepository<Entity클래스, PK 타입>를 상속하면 기본적인 CRUD메소드가 자동으로 생성
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
