457da714688ab183a18a61674d73765c46a70cde
sylph611 <ccg0611a@gmail.com>
2020-06-16 03:43:34 +0900

    2020.06.16 공부 issue -> userName변수가 윈도우계정의 아이디로 사용되고있어 수정함 signedUserName

--- src/main/java/com/sylph/ex/springboot/web/IndexController.java
+++ src/main/java/com/sylph/ex/springboot/web/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
         model.addAttribute("posts",postsService.findAllDesc());
 
         if (user != null){
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("signedUserName", user.getName());
         }
         return "index";
     }
